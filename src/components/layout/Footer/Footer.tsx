e4d0aa5bbc7ee4c7e2b4e96b4a0dba607b6d1eea
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-06-24 11:59:00 +0300

    found new icons

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -2,9 +2,9 @@ import { Contacts } from '../../../constants/Contacts';
 import '../../../styles/global.scss';
 import { IconLink } from '../../common/IconLink/IconLink';
 import styles from './Footer.module.scss';
-import { FaTelegram, FaInstagram, FaBook } from "react-icons/fa";
-import { TbWorld } from "react-icons/tb";
+import { FaInstagram } from "react-icons/fa";
 import { FaTelegramPlane } from "react-icons/fa";
+import { MdLibraryBooks } from "react-icons/md";
 
 export const Footer = () => {
   return (
@@ -12,7 +12,7 @@ export const Footer = () => {
       <div className='container'>
         <IconLink link={Contacts.TELEGRAM} icon={<FaTelegramPlane />} iconSize="md" iconColor="dark" />
         <IconLink link={Contacts.INSTAGRAM} icon={<FaInstagram />} iconSize="md" iconColor="dark" />
-        <IconLink link={Contacts.B17} icon={<TbWorld />} iconSize="md" iconColor="dark" />
+        <IconLink link={Contacts.B17} icon={<MdLibraryBooks />} iconSize="md" iconColor="dark" />
       </div>
     </footer>
   );
