f54b555e33dd121519ff88ada09d0e4e724206e7
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-06-27 14:15:29 +0300

    made post be able as accordion

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,13 +1,17 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 import styles from './Post.module.scss';
 import { FaLink } from 'react-icons/fa6';
 import author from '../../assets/images/user/avatar.png';
+import { IoIosArrowUp } from 'react-icons/io';
 
 interface PostProps {
+  id: number;
   date: string;
   title: string;
   text: string;
   url: string;
+  onClick: () => void;
+  isOpen: boolean;
 }
 
 interface ParagraphProps {
@@ -33,30 +37,57 @@ const formatText = (text: string) => {
   ));
 };
 
-export const Post: FC<PostProps> = ({ date, title, text, url }) => {
+export const Post: FC<PostProps> = ({
+  date,
+  title,
+  text,
+  url,
+  onClick,
+  isOpen,
+}) => {
+  const itemRef = useRef<HTMLDivElement>(null);
+
   return (
-    <div className={styles.wrapper}>
+    <button className={styles.wrapper} onClick={() => onClick()}>
       <div className={styles.header}>
         <div className={styles.author}>
           <img src={author} alt="author" />
         </div>
-        <h3 className={styles.title}>{title}</h3>
+        <h3 className={`${styles.title} ${isOpen ? styles.openTitle : ''}`}>
+          {title}
+        </h3>
         <span className={styles.date}>{date}</span>
+        <span>
+          <IoIosArrowUp
+            className={`${styles.arrow} ${isOpen ? styles.openArrow : ''}`}
+          />
+        </span>
       </div>
-      <div className={styles.content}>{formatText(text)}</div>
-      {url && (
-        <div className={styles.footer}>
-          <a
-            href={url}
-            target="_blank"
-            rel="noreferrer"
-            className={styles.link}
-          >
-            <FaLink className={styles.icon} />
-            <span>Перейти на сайт</span>
-          </a>
+      <div
+        className={styles.collapse}
+        style={
+          isOpen
+            ? { height: `${itemRef.current?.scrollHeight + 15}px` }
+            : { height: '0px' }
+        }
+      >
+        <div className={styles.content} ref={itemRef}>
+          {formatText(text)}
+          {url && (
+            <div className={styles.footer}>
+              <a
+                href={url}
+                target="_blank"
+                rel="noreferrer"
+                className={styles.link}
+              >
+                <FaLink className={styles.icon} />
+                <span>Узнать больше</span>
+              </a>
+            </div>
+          )}
         </div>
-      )}
-    </div>
+      </div>
+    </button>
   );
 };
--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { TPost } from '../../types/types';
 import { Post } from '../Post/Post';
 import styles from './PostsList.module.scss';
@@ -12,13 +12,24 @@ interface PostsListProps {
 export const PostsList: FC<PostsListProps> = ({
   posts,
   isLoading,
-  isError,
+  // isError,
 }) => {
+  const [openId, setOpenId] = useState<number | null>(null);
+
   return (
     <section className={styles.wrapper}>
       {isLoading && <h2>Loading..</h2>}
-      {posts.length &&
-        posts?.map((post: TPost) => <Post key={post.id} {...post} />)}
+      {posts.length > 0 &&
+        posts?.map((post: TPost) => (
+          <Post
+            key={post.id}
+            {...post}
+            onClick={() =>
+              post.id === openId ? setOpenId(null) : setOpenId(post.id)
+            }
+            isOpen={post.id === openId}
+          />
+        ))}
     </section>
   );
 };
