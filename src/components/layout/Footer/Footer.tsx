15a8b45744ff6b81e239e5b3bad1e0a56f4b116a
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-09 14:49:39 +0300

    made qrcode

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -5,30 +5,36 @@ import styles from './Footer.module.scss';
 import { FaInstagram } from 'react-icons/fa';
 import { FaTelegramPlane } from 'react-icons/fa';
 import { MdLibraryBooks } from 'react-icons/md';
+import qrCode from '../../../assets/images/qr/qr-code.png';
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className="container">
         <div className={styles.wrapper}>
-          <IconLink
-            link={Contacts.TELEGRAM}
-            icon={<FaTelegramPlane />}
-            size="md"
-            text="Telegram"
-          />
-          <IconLink
-            link={Contacts.INSTAGRAM}
-            icon={<FaInstagram />}
-            size="md"
-            text="Instagram"
-          />
-          <IconLink
-            link={Contacts.B17}
-            icon={<MdLibraryBooks />}
-            size="md"
-            text="B17"
-          />
+          <div className={styles.socials}>
+            <IconLink
+              link={Contacts.TELEGRAM}
+              icon={<FaTelegramPlane />}
+              size="md"
+              text="Telegram"
+            />
+            <IconLink
+              link={Contacts.INSTAGRAM}
+              icon={<FaInstagram />}
+              size="md"
+              text="Instagram"
+            />
+            <IconLink
+              link={Contacts.B17}
+              icon={<MdLibraryBooks />}
+              size="md"
+              text="B17"
+            />
+          </div>
+          <div className={styles.qrWrapper}>
+            <img className={styles.qr} src={qrCode} alt="qrCode" />
+          </div>
         </div>
       </div>
     </footer>
